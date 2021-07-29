333ec312571663e71dc0fb0aeb783ba15e52f37f
partharoyy <partharoyy88@gmail.com>
2021-07-29 17:33:22 +0530

    disabled firebase for now

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,22 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAZBrBdcIfa9NBANjIs7QDZHHczs1zJmQs",
-    authDomain: "notes-e203c.firebaseapp.com",
-    projectId: "notes-e203c",
-    storageBucket: "notes-e203c.appspot.com",
-    messagingSenderId: "905889611505",
-    appId: "1:905889611505:web:2b5982e6f2dc584b39f350"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAZBrBdcIfa9NBANjIs7QDZHHczs1zJmQs",
+//     authDomain: "notes-e203c.firebaseapp.com",
+//     projectId: "notes-e203c",
+//     storageBucket: "notes-e203c.appspot.com",
+//     messagingSenderId: "905889611505",
+//     appId: "1:905889611505:web:2b5982e6f2dc584b39f350"
+// };
 
 // Initialize Firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
-const db = firebase.firestore()
-const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+const db = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
-export { db, timestamp }
+export { db, timestamp };
